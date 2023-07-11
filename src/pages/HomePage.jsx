da2b4b13f7fe78d6fe03c5e17d7037f5cf7fe964
Matheus <matheusvmja@gmail.com>
2023-07-11 04:45:35 -0300

    Post/cadastro almost finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import { BiExit } from "react-icons/bi"
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai"
+import { useEffect } from "react"
+import axios from "axios"
 
 export default function HomePage() {
   return (
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,49 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
+import axios from "axios"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState, useNavigate } from "react"
 
 export default function SignUpPage() {
+
+  const [cadastro, setCadastro] = useState({})
+
+  function signUp() {
+
+    return (post) => {
+      axios.post(`${import.meta.env.VITE_API_URL}/cadastro`, post)
+        .then(res => useNavigate("/"))
+        .catch(err => alert(err.response.data))
+    }
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+    setCadastro(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (cadastro.password !== cadastro.confirmPassword) return alert("confirme sua senha")
+
+    delete cadastro.confirmPassword
+    signUp(cadastro)
+    console.log(cadastro)
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={handleSubmit}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input required placeholder="Nome" type="text" name="name" value={cadastro.name || ""} onChange={handleChange} />
+        <input required placeholder="E-mail" type="email" name="email" value={cadastro.email || ""} onChange={handleChange} />
+        <input required minLength={3} placeholder="Senha" type="password" autoComplete="new-password" name="password" value={cadastro.password || ""} onChange={handleChange} />
+        <input required placeholder="Confirme a senha" type="password" autoComplete="new-password" name="confirmPassword" value={cadastro.confirmPassword || ""} onChange={handleChange} />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
